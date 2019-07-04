Fixed calculation for hand movement
array begins like this
[x,width,height,y]

AND NOT LIKE THIS
[x,y,width,height]

tensorflow_api/object_detection/stream_detect.py
@@ -1,7 +1,7 @@
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 import numpy as np
-
+import pyautogui
 import tensorflow as tf
 
 import cv2
@@ -10,6 +10,8 @@ cap = cv2.VideoCapture(0)
 MODEL = './trained_model/open_hand/frozen_inference_graph_v2.pb'
 LABELS = './trained_model/open_hand/label_map.pbtxt'
 NUM_CLASSES = 1
+
+pyautogui.moveTo(1920/2,1080/2)
 
 # Load Tensorflow model
 detection_graph = tf.Graph()
@@ -34,7 +36,7 @@ with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
         while True:
             ret, image_np = cap.read()
-
+            pyautogui.FAILSAFE = False
             image_np_expanded = np.expand_dims(image_np, axis=0)
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
             boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
@@ -53,16 +55,25 @@ with detection_graph.as_default():
                 np.squeeze(scores),
                 categories_index,
                 use_normalized_coordinates=True,
-                min_score_thresh=0.65,
+                min_score_thresh=0.8,
                 line_thickness=8)
 
             for c, score in zip(classes[0], scores[0]):
-                if(score > 0.65):
+                category = categories[int(c) - 1]['name']
+                if(score > 0.7 and category == 'openhand'):
                     print(int(c), ':', score)
-                    print(categories[int(c) - 1]['name'])
+              
                     print(boxes[0][0])
+                    x = ((boxes[0][0][1]-boxes[0][0][0])/2) + boxes[0][0][0]
+                    y = ((boxes[0][0][2]-boxes[0][0][3])/2) + boxes[0][0][3]
+                    pyautogui.moveTo(x*1920,y*1080)
+                    # print(boxes[0][0][1] * 800)
+                    # print(boxes[0][0][0] * 600)
+
 
             cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
+            # main()
+            